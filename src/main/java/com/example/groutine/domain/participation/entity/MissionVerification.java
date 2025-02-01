89e08ab198ed53be18dfe05786451aa173875d05
shimfff <shimseawon1510@gmail.com>
2025-02-01 21:19:14 +0900

    :bug: fix : 엔티티 어노테이션 추가
    Related to: #1

--- src/main/java/com/example/groutine/domain/participation/entity/MissionVerification.java
+++ src/main/java/com/example/groutine/domain/participation/entity/MissionVerification.java
@@ -4,12 +4,14 @@ import com.example.groutine.domain.challenge.entity.ChallengeMission;
 import com.example.groutine.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Setter
+@Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MissionVerification {
 
