c892cb781202b805734050624c495fa88ec4ad59
shimfff <shimseawon1510@gmail.com>
2025-02-01 21:04:37 +0900

    :sparkles: feat : ChallengeMission 엔티티 생성
    Related to: #1

--- src/main/java/com/example/groutine/domain/challenge/entity/ChallengeMission.java
+++ src/main/java/com/example/groutine/domain/challenge/entity/ChallengeMission.java
@@ -1,4 +1,42 @@
 package com.example.groutine.domain.challenge.entity;
 
-public class ChallengeMission {
+import com.example.groutine.domain.participation.entity.MissionVerification;
+import com.example.groutine.global.common.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChallengeMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    private String title;
+
+    @Setter
+    private String content;
+
+    @Setter
+    private String verifyGuide;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @OneToMany
+    private List<MissionVerification> missionVerificationList = new ArrayList<>();
+
+    @Builder
+    public ChallengeMission(String title, String content, String verifyGuide) {
+        this.title = title;
+        this.content = content;
+        this.verifyGuide = verifyGuide;
+    }
 }
