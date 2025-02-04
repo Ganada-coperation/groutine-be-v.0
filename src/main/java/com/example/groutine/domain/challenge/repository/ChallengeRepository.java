0db6f19318bd4d230e46b4d2bbf0cb1b2e324acb
shimfff <shimseawon1510@gmail.com>
2025-02-05 03:30:04 +0900

    :sparkles: feat: 챌린지 관련 레포지토리 생성
    Relate to: #4

---
+++ src/main/java/com/example/groutine/domain/challenge/repository/ChallengeRepository.java
@@ -0,0 +1,7 @@
+package com.example.groutine.domain.challenge.repository;
+
+import com.example.groutine.domain.challenge.entity.Challenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
+}
