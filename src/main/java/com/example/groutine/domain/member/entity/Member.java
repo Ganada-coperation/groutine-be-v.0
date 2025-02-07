76c8b83802a19f5518b4287b969b1111a91dc03d
shimfff <shimseawon1510@gmail.com>
2025-02-07 16:27:50 +0900

    :bug: fix : 연관관계 설정 수정
    Relate to: #4

--- src/main/java/com/example/groutine/domain/member/entity/Member.java
+++ src/main/java/com/example/groutine/domain/member/entity/Member.java
@@ -56,7 +56,7 @@ public class Member extends BaseEntity {
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<ChallengeMissionVerification> challengeMissionVerificationList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL)
     private List<Challenge> challengeList = new ArrayList<>();
 
 
