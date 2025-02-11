4d9f5ef3ccf8997ad59b80e8de46bc7b66770761
shimfff <shimseawon1510@gmail.com>
2025-02-11 21:53:55 +0900

    :sparkles: feat : 쿼리문 작성
    Relate to: #11

--- src/main/java/com/example/groutine/domain/mission/repository/ChallengeMissionVerificationRepository.java
+++ src/main/java/com/example/groutine/domain/mission/repository/ChallengeMissionVerificationRepository.java
@@ -6,6 +6,7 @@ import com.example.groutine.domain.mission.entity.ChallengeMissionVerification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,9 +22,11 @@ public interface ChallengeMissionVerificationRepository extends JpaRepository<Ch
 
     // todo : 최적화 필요
     // 챌린지 아이디와 생성일자로 미션 인증 조회
+    @Query("SELECT cmv FROM ChallengeMissionVerification cmv WHERE cmv.challengeMember.challenge.id = :challengeId AND cmv.createdAt BETWEEN :atStartOfDay AND :atTime")
     List<ChallengeMissionVerification> findByChallengeMember_Challenge_ChallengeIdAndCreatedAtBetween(Long challengeId, Object atStartOfDay, Object atTime, Pageable pageable);
 
     // 해당 날짜와 특정 미션 대한 내 미션 인증이 있는지 확인
+    @Query("SELECT CASE WHEN COUNT(cmv) > 0 THEN TRUE ELSE FALSE END FROM ChallengeMissionVerification cmv WHERE cmv.challengeMember = :challengeMember AND cmv.createdAt BETWEEN :startOfDay AND :endOfDay AND cmv.challengeMission.challenge.id = :challengeMissionId")
     boolean existsByChallengeMemberAndCreatedAtBetweenAndChallengeMission_ChallengeMissionId(
             ChallengeMember challengeMember,
             LocalDateTime startOfDay,
